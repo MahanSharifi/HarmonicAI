8268a6347cd24a6ce51af2e3355122851f0fc04d
Eden Chan <edenchan717@gmail.com>
2024-03-12 15:22:07 -0400

    add marker with default user data

--- src/app/_components/get-google-maps.tsx
+++ src/app/_components/get-google-maps.tsx
@@ -2,51 +2,76 @@
 
 
 import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, InfoWindow, Marker, useJsApiLoader } from '@react-google-maps/api';
 import { env } from '~/env';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+    width: '800px',
+    height: '800px'
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523
-};
+    lat: 43.4718664459, 
+    lng: -80.5230112413
+  };
+
 
 function GoogleMapsComponent() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
-  })
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+    })
 
-  const [map, setMap] = React.useState(null)
+    const [map, setMap] = React.useState(null)
 
-  const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
+    const onLoad = React.useCallback(function callback(map) {
+        // This is just an example of getting and using the map instance!!! don't just blindly copy!
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
 
-    setMap(map)
-  }, [])
+        setMap(map)
+    }, [])
 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, [])
 
-  return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+    const lat = -3.745
+    const lng = -38.523
+    const title = ' test laurier'
+    const options =  {
+        backgroundColor:'red',
+        styles : [
+            {
+                featureType: 'poi',
+                elementType: 'labels',
+                stylers: [{ visibility: 'off' }] 
+            }
+        ]
+    } 
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={20}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+            options={options}
+        >
+            { /* Child components, such as markers, info windows, etc. */}
+            <Marker
+                position={center}
+                title={title}>
+
+                     <InfoWindow>
+                        <h1 className="text-black">Eden Chan</h1>
+                     </InfoWindow>
+                </Marker>
+
+            <></>
+        </GoogleMap>
+    ) : <></>
 }
 
 export default React.memo(GoogleMapsComponent)
